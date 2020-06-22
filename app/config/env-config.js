1e7a73819af53a1de6aee606fe47234ad168f12d
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-06-21 19:57:13 -0500

    ➕ADD: Parametros
    Parametros de configuracion para conectar a MongoDB Atlas
    
    Issue #5

--- app/config/env-config.js
+++ app/config/env-config.js
@@ -1,6 +1,15 @@
 const APP_PORT = process.env.PORT || 3000;
 
+const USER_DB = process.env.USER_DB || "pokemon";
+const PASS = process.env.PASS || "p0k3m0n.";
+const DB = process.env.DB || "api";
+
 // variables de entorno
 module.exports = {
     "process.env.PORT": APP_PORT,
+    "process.env.ODBC": `mongodb+srv://${USER_DB}:${PASS}@cluster0-eke4y.mongodb.net/${DB}?retryWrites=true&w=majority`,
+    "process.env.CONFIG": {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
 };
