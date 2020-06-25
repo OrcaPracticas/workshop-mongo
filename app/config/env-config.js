9280e07bf330ae20a10d1ff5535babc744f13ba6
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-06-25 00:58:06 -0500

    🛠 FIX: Cambio de puerto
    Se modifica la variable quetoma el puertto esto por cuestiones de condiguracion de Heroku.
    
    Issue #14

--- app/config/env-config.js
+++ app/config/env-config.js
@@ -1,11 +1,8 @@
-const APP_PORT = process.env.PORT || 3000;
-
 const { USER_DB, PASS, DB, HEROKU } = process.env;
 const HOST = HEROKU || `http://localhost:${APP_PORT}`;
 
 // variables de entorno
 module.exports = {
-    "process.env.APP_PORT": APP_PORT,
     "process.env.ODBC": `mongodb+srv://${USER_DB}:${PASS}@cluster0-eke4y.mongodb.net/${DB}?retryWrites=true&w=majority`,
     "process.env.HOST": HOST,
     "process.env.README": "https://github.com/OrcaPracticas/workshop-mongo/blob/develop/README.md",
